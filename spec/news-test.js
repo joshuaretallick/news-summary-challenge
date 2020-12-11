d581800a135083017275f1a02b9a1b20aaadeaaf
Joshua Retallick <josh@old-empire.co.uk>
2020-12-11 16:33:08 +0000

    First test passed and set up framework in Jasmine

--- spec/news-test.js
+++ spec/news-test.js
@@ -1,1 +1,15 @@
 'use strict';
+
+describe('News', function() {
+
+    let news;
+
+    beforeEach(function() {
+        news = new News();
+    });
+
+    it('displays the news', function() {
+        expect(news.getCurrentNews()).toBe(true);
+    });
+
+});
--- src/news.js
+++ src/news.js
@@ -1,1 +1,13 @@
 'use strict';
+
+class News{
+
+    constructor() {
+        this.news = true;
+    };
+
+    getCurrentNews() {
+      return this.news;
+    };
+
+}
